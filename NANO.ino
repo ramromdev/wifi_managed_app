cf06f0c3e6fbcab1bfc3d14d905f673048c4dc0d
RamiC <devneirology@protonmail.com>
2018-09-20 17:54:33 +0200

    Eliminación de alguna redundancia

--- NANO.ino
+++ NANO.ino
@@ -1,6 +1,4 @@
 /*
-Sensor de proximidad y al ser inferior a 10cm 
-envia un pulso de alarma por  el pin 13
 
  HC-SR04 conexiones:
   VCC al arduino 5v 
@@ -12,9 +10,6 @@ envia un pulso de alarma por  el pin 13
 const int echo1 = 3;
 const int trig1 = 2;
 
-//ULTRASONIDOS 2
-//const int echo2 = 8;
-//const int trig2 = 7;
 
 char nivel='0';
 long duracion, distancia, duracion2, distancia2;
@@ -23,9 +18,7 @@ void setup() {
   Serial.begin (115200);       // inicializa el puerto seria a 115200 baudios
   pinMode(echo1, INPUT);     // define el pin 3 como entrada (echo)
   pinMode(trig1, OUTPUT);    // define el pin 2 como salida  (triger)
-  //pinMode(echo2, INPUT);     // define el pin 8 como entrada (echo)
-  //pinMode(trig2, OUTPUT);    // define el pin 7 como salida  (triger)   
-   
+  
   }
   
 void loop() {
@@ -44,19 +37,16 @@ void loop() {
  
   //GESTIÓN PARA EL ULTRASONIDOS 1
   if (distancia <= 70 && distancia >= 50){
-         nivel='1';
-         Serial.write(nivel);
+         nivel='1';         
     }                    
   if (distancia <= 49 && distancia >= 35){
-         nivel='2';
-         Serial.write(nivel);
+         nivel='2';         
     }  
   if (distancia <= 34 && distancia >= 20){
          nivel='3';
-         Serial.write(nivel);
     }
    else{
-    Serial.write(nivel);                                      
+    nivel=nivel;                                      
    }
-                 
+   Serial.write(nivel);            
 }
